7f670f8376c8bcb857d322ba2e9f608ca6e7b105
Rijish Ganguly <rijish96@gmail.com>
2019-03-17 18:28:50 -0400

    Added basic tables

--- postgresql/main.cpp
+++ postgresql/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <pqxx/pqxx>
-
+#include <string>
 #include "exerciser.h"
 
 using namespace std;
@@ -8,11 +8,62 @@ using namespace pqxx;
 
 
 
+void drop_existing_tables(connection * C){
 
-void add_player(Connection *  C){};
 
-void add_
+  string sql = "DROP TABLE IF EXISTS PLAYER, TEAM, STATE, COLOR;";
+  work W(*C);
+  W.exec(sql);
+  W.commit();
+  
 
+};
+
+
+void add_player(connection * C){
+
+  string sql = "CREATE TABLE PLAYER("  \
+      "PLAYER_ID    SERIAL PRIMARY KEY     NOT NULL," \
+      "TEAM_ID      INT                    NOT NULL," \
+      "UNIFORM_NUM  INT                    NOT NULL," \
+      "FIRST_NAME   TEXT                   NOT NULL," \
+      "LAST_NAME    TEXT                   NOT NULL," \
+      "MPG          INT                    NOT NULL," \
+      "PPG          INT                    NOT NULL," \
+      "RPG          INT                    NOT NULL," \
+      "APG          INT                    NOT NULL," \
+      "SPG          REAL                   NOT NULL," \
+      "BPG          REAL                   NOT NULL);";  
+         
+
+    work W(*C);
+    W.exec(sql);
+    W.commit();
+    cout << "Table Player has been created" << endl;
+   
+};
+
+
+
+void add_team (connection * C){
+
+ string sql = "CREATE TABLE TEAM("  \
+      "TEAM_ID      SERIAL PRIMARY KEY     NOT NULL," \
+      "NAME         TEXT                   NOT NULL," \
+      "STATE_ID     INT                    NOT NULL," \
+      "COLOR_ID     INT                    NOT NULL," \
+      "WINS         INT                    NOT NULL," \
+      "LOSSES       INT                    NOT NULL);";
+
+    work W(*C);
+    W.exec(sql);
+    W.commit();
+    cout << "Table Team has been created" << endl;
+ 
+
+};
+//void add_state(Connection * C){}
+//void add_color(Connection * C){};
 
 
 
@@ -42,7 +93,9 @@ int main (int argc, char *argv[])
   //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
   //      load each table with rows from the provided source txt files
 
-
+  drop_existing_tables(C);
+  add_player(C);
+  add_team(C);
   exercise(C);
 
 
