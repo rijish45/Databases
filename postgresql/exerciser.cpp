a5069bcc610949a0c10ffdaab0c9bc33840693de
Rijish Ganguly <rijish96@gmail.com>
2019-03-19 19:38:36 -0400

    Done

--- postgresql/exerciser.cpp
+++ postgresql/exerciser.cpp
@@ -11,34 +11,37 @@ void exercise(connection *C)
 	   0, 0, 0,
 	   0, 0, 0,
 	   0, 0, 0);
-
+  cout << endl;
   query1(C,1, 0, 40,
 	   1, 0, 40,
 	   0, 0, 0,
 	   0, 0, 0,
 	   1, 20, 0,
 	   0, 0, 0);
-
-   query1(C,0, 0, 0,
+  cout << endl;
+  query1(C,0, 0, 0,
 	   1, 0, 40,
 	   1, 0, 40,
 	   0, 0, 0,
 	   1, 20, 0,
 	   0, 0, 0);
-
-   cout << endl;
+  cout << endl;
 
    
 cout << "Testing query 2" << endl;
 query2(C, "Red");
+cout << endl;
 query2(C, "Orange");
+cout << endl;
 query2(C, "Green");
 cout << endl;
  
 
 cout << "Testing query 3" << endl;
 query3(C, "NCSU");
+cout << endl;
 query3(C,"Duke");
+cout << endl;
 query3(C, "Clemson");
 cout << endl;
 
@@ -46,6 +49,7 @@ cout << endl;
 
 cout << "Testing query 4" << endl; 
 query4(C, "NC", "LightBlue");
+cout << endl;
 query4(C, "NY", "Orange");
 cout << endl;
  
@@ -53,7 +57,9 @@ cout << endl;
 
 cout << "Testing query 5" << endl;
 query5(C, 5);
+cout << endl;
 query5(C, 9);
+cout << endl;
 query5(C, 12);
 cout << endl;
 
